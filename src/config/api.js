233098022af4521a9cab8279d5b31bc10ed79671
Harsh <harsh.pandey@degreefyd.com>
2026-01-22 11:15:28 +0530

    watsaap on student open

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
-export const BASE_URL = "http://localhost:3031/v1"
+// export const BASE_URL = "http://localhost:3031/v1"
 
 
 // ---prod one---
-// export const BASE_URL = "https://lms-regular.degreefyd.com/v1"
+export const BASE_URL = "https://lms-regular.degreefyd.com/v1"
 
 
 //-- test one---
